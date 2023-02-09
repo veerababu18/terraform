f4083ba72a86c31ada7c41f0380c668852d0ad3a
nasinghn <33350388+nasinghn@users.noreply.github.com>
2023-02-09 10:41:14 +0000

    created terraform config file

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  #  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = "ux-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0603cbe34fd08cb81"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+output "app_server" {
+  value = "${aws_instance.app_server.public_ip}"
+}
